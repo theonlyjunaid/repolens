e09f7bb1c0876ee918b7eaeaffa4e238fde0e2c5
Talha Ansari <talhaansari1606@gmail.com>
2025-02-11 17:15:23 +0530

    update

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,18 +1,13 @@
-import {withAuth} from "@kinde-oss/kinde-auth-nextjs/middleware";
-import { NextRequest , NextResponse} from "next/server";
+import { withAuth } from "@kinde-oss/kinde-auth-nextjs/middleware";
+import { NextRequest } from "next/server";
+
 export default function middleware(req : NextRequest) {
-
-
-  if (req.nextUrl.pathname === "/sync-user-to-db") {
-    return NextResponse.next();
-  }
-  return withAuth(req, {
-    isReturnToCurrentPage: true,
-    loginPage: "/",
-  });
+  return withAuth(req);
 }
+
 export const config = {
-  matcher: []
+  matcher: [
+    // Run on everything but Next internals and static files
+    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+  ]
 };
-
-
