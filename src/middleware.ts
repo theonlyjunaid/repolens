fbc6e46c55e84934f567e2310eb73626896855ad
Talha Ansari <talhaansari1606@gmail.com>
2025-02-11 17:22:44 +0530

    update

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,13 +1,16 @@
-import { withAuth } from "@kinde-oss/kinde-auth-nextjs/middleware";
-import { NextRequest } from "next/server";
-
+import {withAuth} from "@kinde-oss/kinde-auth-nextjs/middleware";
+import { NextRequest , NextResponse} from "next/server";
 export default function middleware(req : NextRequest) {
-  return withAuth(req);
-}
 
+
+
+  return withAuth(req, {
+    isReturnToCurrentPage: true,
+    loginPage: "/",
+  });
+}
 export const config = {
-  matcher: [
-    // Run on everything but Next internals and static files
-    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-  ]
+  matcher: []
 };
+
+
